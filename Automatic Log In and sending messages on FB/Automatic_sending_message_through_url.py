dfbeb00189204ad53cf4920e9a1cc8873a39d17f
Durjoy Barua <142112376+durjoybarua5327@users.noreply.github.com>
2025-01-29 18:10:11 +0600

    Update Automatic_sending_message_through_url.py

--- Automatic Log In and sending messages on FB/Automatic_sending_message_through_url.py
+++ Automatic Log In and sending messages on FB/Automatic_sending_message_through_url.py
@@ -1,10 +1,8 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
 path = "K:/chromedriver-win64/chromedriver.exe"
 s = Service(path)
 
